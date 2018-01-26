02bbfbeb399acdd11f3293f1a63933629f134624
KK <krishudr@gmail.com>
2018-01-26 09:33:13 -0500

    UAT Changes

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.Write("Hello World");
-
+            Console.Write("Hello World - UAT");
         }
     }
 }
